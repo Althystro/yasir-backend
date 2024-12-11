de4a80951b0c644f12fa9ad96a6fc6a22b207412
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-12-11 16:51:12 +0300

    trying

---
+++ src/main/java/com/example/yasir/YasirBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.yasir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasirBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasirBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yasir/YasirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yasir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
