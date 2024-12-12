a4ac132bf7f856edbf17c51e92ac369092f08391
Abdullah Alhumaidhan <abdullahkh56@gmail.com>
2024-12-12 14:48:42 +0300

    Some payment methods

---
+++ src/main/java/com/example/yasir/payments/entity/PaymentEntity.java
@@ -0,0 +1,34 @@
+package com.example.yasir.payments.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class PaymentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date date;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/example/yasir/payments/repository/PaymentPlanRepository.java
@@ -0,0 +1,15 @@
+package com.example.yasir.payments.repository;
+
+import com.example.yasir.payments.entity.PaymentPlanEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentPlanRepository extends JpaRepository<PaymentPlanEntity, Long> {
+    @Query("SELECT p FROM PaymentPlan p WHERE p.financer.id = :financerId")
+    List<PaymentPlanEntity> findByFinancerId(@Param("financerId") Long financerId);
+}
